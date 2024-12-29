f0295525f92ffc3d846ca0f3cefebe8473598f60
Omer Bresinski <omerbres@gmail.com>
2024-12-29 17:14:58 +0000

    insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -11,5 +12,6 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
+    <SpeedInsights />
   </StrictMode>
 );

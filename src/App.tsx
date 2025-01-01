6b3cb76e565a014829b3c7ac8c952fcfdc99cd6b
Omer Bresinski <omerbres@gmail.com>
2025-01-01 02:20:15 +0000

    mobile spacing

--- src/App.tsx
+++ src/App.tsx
@@ -72,14 +72,18 @@ function App() {
             <motion.form
               onSubmit={handleSubmit}
               className={`w-full flex flex-col items-center ${
-                isMobile ? "overflow-hidden pt-[135px] pb-8" : "pt-32 pb-8"
+                isMobile ? "overflow-hidden pt-[120px] pb-8" : "pt-32 pb-8"
               } font-serif ${isMobile ? "px-[42px]" : "px-0"}`}
               initial={{ opacity: 0 }}
               exit={{ opacity: 0 }}
               animate={{ opacity: 1 }}
             >
               <BrandIcon />
-              <div className="text-center max-w-3xl pt-2 mt-14 mb-[34px]">
+              <div
+                className={`text-center max-w-3xl pt-2 ${
+                  isMobile ? "mt-11 mb-6" : "mt-14 mb-[34px]"
+                }`}
+              >
                 <AnimatePresence mode="wait">
                   {!isFetching && !movie && (
                     <Typewriter onClick={handleTypewriterClick} />
